a72a348ce544dac112f690c3c8453d77720fe75b
Vladimir Orlov <orelik392@gmail.com>
2016-12-17 16:16:20 +0200

    pointer to pointer and address of pointer

--- point2point/point2point/point2point.cpp
+++ point2point/point2point/point2point.cpp
@@ -9,6 +9,19 @@ using namespace  std;
 
 int main()
 {
+	int var = 123;
+	cout << "var = " << var << endl;
+
+	int * ptrvar = &var; // указатель на переменную var
+	cout << "*ptrvar = " << *ptrvar << endl;
+	cout << "address of ptrvar ( ptrvar ) = " << ptrvar << endl;
+
+
+	int **ptr_ptrvar = &ptrvar; //указатель на указатель var
+	cout << "**ptr_ptrvar = " << ** ptr_ptrvar << endl;
+	cout << "address of ptr_ptrvar ( ptr_ptrvar ) = " << ptr_ptrvar << endl;
+
+	
 
 	system("pause");
     return 0;

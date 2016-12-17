6b9ddd94961a65de27099044cb371400c7806e7c
Vladimir Orlov <orelik392@gmail.com>
2016-12-17 16:06:17 +0200

    first commit

---
+++ point2point/point2point/point2point.cpp
@@ -0,0 +1,16 @@
+// point2point.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace  std;
+
+
+int main()
+{
+
+	system("pause");
+    return 0;
+}
+

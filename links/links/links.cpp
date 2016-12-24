84aad0ccddb9119dad83ca71bbe0af9037cad690
Vladimir Orlov <orelik392@gmail.com>
2016-12-24 21:21:08 +0200

    best

--- links/links/links.cpp
+++ links/links/links.cpp
@@ -6,6 +6,10 @@
 
 using namespace std;
 
+int sum_by_value(int); 
+int sum_by_reference(int &);
+int sum_by_pointer(int *);
+
 int main()
 {
 	int value = 15;
@@ -19,7 +23,34 @@ int main()
 	cout << "value = " << value << endl;
 	cout << "reference  = " << refer << endl;
 
+
+	cout << "sum_by_value  = \t" << sum_by_value(value) << endl;
+	cout << "value = \t" << value << endl;
+
+	cout << "sum_by_reference  = \t" << sum_by_reference(value) << endl;
+	cout << "value = \t" << value << endl;
+
+	cout << "sum_by_pointer  = \t" << sum_by_pointer(&value) << endl;
+	cout << "value = \t" << value << endl;
+
 	system("pause");
     return 0;
 }
 
+int sum_by_value (int value) // функция принимающая аргумент по значению
+{
+	value += value;
+	return value;
+}
+
+int sum_by_reference (int &reference) // функция принимающая аргумент по ссылке
+{
+	reference += reference;
+	return reference;
+}
+
+int sum_by_pointer (int *ptrvalue) // функция принимающая аргумент через указатель
+{
+	*ptrvalue += *ptrvalue; // арифметика с указателем
+	return *ptrvalue;
+}

6cec256d48593b3b9f8dba8410a32e16f1408caf
Vladimir Orlov <orelik392@gmail.com>
2017-01-13 12:23:24 +0200

    commit 1

---
+++ Comment/Comment/Comment.cpp
@@ -0,0 +1,30 @@
+// Comment.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+// attach library
+#include <iostream>;
+
+using namespace std;
+
+
+int main()
+{
+	// comment1
+	/*
+		Hi.
+		This is Commment!
+	*/
+
+	int i = 4;
+	char c;
+
+	int r = 5;
+	char d = 'd';
+
+	int value = i + r;
+
+	system("pause");
+    return 0;
+}
+
